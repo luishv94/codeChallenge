52ccefd92579ba5fe7f9050344b2272c94af9ae2
Luis Vargas <khxz982@Sierras-MacBook-Pro.local>
2020-02-21 13:58:49 -0600

    buttons and minor css changes

--- frontend-app/src/components/LandingPage.js
+++ frontend-app/src/components/LandingPage.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { ConnectedNavBar } from '../containers/ConnectedNavBar'
+import { FrequencyCountButton } from './FrequencyCountButton'
+import { PossibleDuplicatesButton } from './PossibleDuplicatesButton'
 import { ConnectedPeopleData } from '../containers/ConnectedPeopleData'
 
 const Page = styled.div`
-  display: grid;
+  display: flex;
   grid-template 46px 1fr / 1fr;
   height: 100%;
   width: 100%;
 `;
 
 const FlexBox = styled.div`
-  display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: left;
 `;
 
 export const LandingPage = () => (
-  <Page>
-    
-    <FlexBox>
-      <ConnectedPeopleData />
-    </FlexBox>
-  </Page>
+  <div>
+    <FrequencyCountButton />
+    <PossibleDuplicatesButton />
+    <Page>
+      <FlexBox>
+        <ConnectedPeopleData />
+      </FlexBox>
+      <FlexBox>
+        <ConnectedPeopleData />
+      </FlexBox>
+    </Page>
+  </div>
 );
---
+++ frontend-app/src/components/PossibleDuplicatesButton.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NAV_HEIGHT = 46;
+const AVATAR_HEIGHT = 40;
+const AVATAR_WIDTH = 40;
+
+const StyledNavBar = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  align-content: center;
+
+  margin: 0px;
+  padding 0px;
+  height: ${NAV_HEIGHT}px;
+  width: 100%;
+`;
+
+let NavText = styled.p`
+  color: #4a90e2;
+  padding-right: 10px;
+  padding-left: 10px;
+  color: white;
+`;
+
+const Badge = styled.div`
+  margin: 3px;
+  display: flex;
+  min-width: 152px;
+  align-items: center;
+  overflow: hidden;
+  border-radius: 7px;
+  border 2px solid #4a90e2;
+  justify-content: center;
+  font-size: 25px;
+  font-weight: bold;
+  background-color: #4a90e2;
+`;
+
+let SignInBadge = () => (
+  <Badge>
+    <a href='/users/auth/salesloft' style={{textDecoration: 'none'}}><NavText>Possible duplicates</NavText></a>
+  </Badge>
+);
+
+export const PossibleDuplicatesButton = () => (
+  <StyledNavBar>
+    <SignInBadge />
+  </StyledNavBar>
+);
