cc02151cd594d91306ec9b6a0e532e7ac022e542
Luis Vargas <khxz982@Sierras-MacBook-Pro.local>
2020-02-21 12:26:45 -0600

    cors problem fixed

--- backendApp/api/people/people.route.js
+++ backendApp/api/people/people.route.js
@@ -3,6 +3,12 @@ const getPeopleController = require('./people.controller');
 
 const router = express.Router();
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 // Get the list of people.
 router.get(
     '/',
