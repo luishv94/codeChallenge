d9dff4e076a7664cd6af5b29120ddebed52351cb
Luis Vargas <khxz982@Sierras-MacBook-Pro.local>
2020-02-21 12:41:39 -0600

    removing unnecesary creds

--- frontend-app/src/actions/AsyncActions.js
+++ frontend-app/src/actions/AsyncActions.js
@@ -1,10 +1,8 @@
 import * as actions from './Actions'
 
-export const creds = { credentials: 'same-origin' };
-
 export function fetchPeople() {
   return dispatch => {
-    return fetch('http://localhost:3000/api/people', creds).
+    return fetch('http://localhost:3000/api/people').
       then(response => response.json(),
             error => console.log('An error occurred.', error)).
       then(json => dispatch(actions.receivePeople(json)));
